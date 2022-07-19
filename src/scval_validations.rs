dfe469e4957766052094f0d8ec04bc8c2ded60c8
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-07-18 20:47:01 -0700

    wip

--- src/scval_validations.rs
+++ src/scval_validations.rs
@@ -111,6 +111,59 @@ mod test {
 
     #[test]
     fn map() {
+        // Values and objects in the data model have a total order. When
+        // comparing two values A and B:
+        //  - If A is a positive int64 and B is not, A is less than B.
+        assert_eq!(
+            ScVal::Object(Some(ScObject::Map(ScMap(
+                vec![
+                    ScMapEntry {
+                        key: ScVal::U63(0),
+                        val: ScVal::U32(1),
+                    },
+                    ScMapEntry {
+                        key: ScVal::U63(1),
+                        val: ScVal::U63(1),
+                    }
+                ]
+                .try_into()
+                .unwrap()
+            ))))
+            .validate(),
+            Ok(())
+        );
+        //  - If A and B are both positive int64 values, they are ordered by the
+        //  normal int64 order.
+        //  - If A and B are both tagged and if A has a lesser tag than B, A is
+        //  less than B.
+        //  - If A and B are both equally tagged, then:
+        //    - If they have tag 0, they are ordered by the normal uint32 order
+        //    on their low 32 bits.
+        //    - If they have tag 1, they are ordered by the normal int32 order
+        //    on their low 32 bits.
+        //    - If they have tag 2, 5 or 6 or 7 they are ordered by the normal
+        //    uint64 order on the zero-extension of their low 60 bits.
+        //    - If they have tag 4 they are ordered by the lexicographical order
+        //    of their Unicode string value.
+        //    - If they have tag 3 they are ordered by calling obj_cmp(A, B)
+        //    which performs deep object comparison.
+        //
+        // Deep object comparison can be accessed by either guest or host: it is
+        // provided to guests as a host function via the host environment
+        // interface. It performs a recursive structural comparison of objects
+        // and values embedded in objects using the following rules:
+        //  - If A and B have different object types, they are ordered by object
+        //  type code.
+        //  - If A and B are boxes, their values are ordered by the value rules
+        //  above.
+        //  - If A and B are vectors, they are ordered by lexicographic
+        //  extension of the value order
+        //  - If A and B are maps, they are ordered lexicographically as ordered
+        //  vectors of (key, value) pairs
+        //  - If A and B are int64 or uint64, they are ordered using the normal
+        //  order for those types
+        //  - If A and B are binary, they are ordered using the lexicograhical
+        //  order of their respective bytes
         assert_eq!(
             ScVal::Object(Some(ScObject::Map(ScMap(
                 vec![

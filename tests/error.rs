ea6926c1e840563163e57d6915b1557664157ab3
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-08-30 14:43:43 -0700

    more test

--- tests/error.rs
+++ tests/error.rs
@@ -4,13 +4,17 @@
 ))]
 #![cfg(all(feature = "std", feature = "serde"))]
 
-use stellar_xdr::{ScError, ScVal};
+use stellar_xdr::{ScError, ScVal, VecM};
 
 #[test]
 fn test_serde_ser() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(
         serde_json::to_string(&ScVal::Error(ScError::Contract(4)))?,
         "{\"error\":{\"contract\":4}}"
+    );
+    assert_eq!(
+        serde_json::to_string(&VecM::<ScVal, 1>::try_from([ScVal::Error(ScError::Contract(4))])?)?,
+        "[{\"error\":{\"contract\":4}}]"
     );
 
     Ok(())

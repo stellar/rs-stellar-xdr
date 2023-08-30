fcdbc363ccc7c4aa1dcf6ec8408eae527322303d
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-08-30 14:50:37 -0700

    more test

--- tests/error.rs
+++ tests/error.rs
@@ -4,7 +4,7 @@
 ))]
 #![cfg(all(feature = "std", feature = "serde"))]
 
-use stellar_xdr::{ScError, ScVal, VecM};
+use stellar_xdr::{ContractEventV0, ScError, ScVal, VecM};
 
 #[test]
 fn test_serde_ser() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,8 +13,17 @@ fn test_serde_ser() -> Result<(), Box<dyn std::error::Error>> {
         "{\"error\":{\"contract\":4}}"
     );
     assert_eq!(
-        serde_json::to_string(&VecM::<ScVal, 1>::try_from([ScVal::Error(ScError::Contract(4))])?)?,
+        serde_json::to_string(&VecM::<ScVal, 1>::try_from([ScVal::Error(
+            ScError::Contract(4)
+        )])?)?,
         "[{\"error\":{\"contract\":4}}]"
+    );
+    assert_eq!(
+        serde_json::to_string(&ContractEventV0 {
+            topics: VecM::try_from([ScVal::Error(ScError::Contract(4))])?,
+            data: ScVal::Void,
+        })?,
+        "{\"topics\":[{\"error\":{\"contract\":4}}],\"data\":\"void\"}"
     );
 
     Ok(())

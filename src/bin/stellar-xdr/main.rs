a29e64b5608e084d3c6def756c412a19c468f7cc
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2023-11-23 02:35:13 -0800

    todo

--- src/bin/stellar-xdr/main.rs
+++ src/bin/stellar-xdr/main.rs
@@ -4,6 +4,8 @@ use stellar_xdr::cli;
 
 fn main() {
     if let Err(e) = cli::run(env::args_os()) {
+        // TODO: Don't treat the 'help' case as an error.
+        // TODO: Remove Box<dyn Error> and replace with concrete errors.
         Error::raw(clap::error::ErrorKind::ValueValidation, e).exit();
     }
 }

5ce68863fc40b55b5eb07ea85e70b1ea91610879
Gleb <gleb@stellar.org>
2025-02-07 15:45:51 -0800

    Add support decoding strings as arguments

--- src/cli/decode.rs
+++ src/cli/decode.rs
@@ -2,10 +2,10 @@ use std::{
     fmt::Debug,
     fs::File,
     io::{stdin, Read},
-    path::PathBuf,
     str::FromStr,
 };
-
+use std::ffi::OsString;
+use std::io::{Cursor, ErrorKind};
 use clap::{Args, ValueEnum};
 use serde::Serialize;
 
@@ -28,21 +28,21 @@ pub enum Error {
 #[derive(Args, Debug, Clone)]
 #[command()]
 pub struct Cmd {
-    /// Files to decode, or stdin if omitted
+    /// Input that could either be strings or files to decode, or stdin if empty
     #[arg()]
-    pub files: Vec<PathBuf>,
+    pub input: Vec<OsString>,
 
     /// XDR type to decode
     #[arg(long)]
     pub r#type: String,
 
     // Input format of the XDR
-    #[arg(long, value_enum, default_value_t)]
-    pub input: InputFormat,
+    #[arg(long = "input", value_enum, default_value_t)]
+    pub input_format: InputFormat,
 
     // Output format
-    #[arg(long, value_enum, default_value_t)]
-    pub output: OutputFormat,
+    #[arg(long = "output", value_enum, default_value_t)]
+    pub output_format: OutputFormat,
 }
 
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, ValueEnum)]
@@ -77,12 +77,12 @@ impl Default for OutputFormat {
 macro_rules! run_x {
     ($f:ident, $m:ident) => {
         fn $f(&self) -> Result<(), Error> {
-            let mut files = self.files()?;
+            let mut input = self.parse_input()?;
             let r#type = crate::$m::TypeVariant::from_str(&self.r#type).map_err(|_| {
                 Error::UnknownType(self.r#type.clone(), &crate::$m::TypeVariant::VARIANTS_STR)
             })?;
-            for f in &mut files {
-                match self.input {
+            for f in &mut input {
+                match self.input_format {
                     InputFormat::Single => {
                         let mut l = crate::$m::Limited::new(f, crate::$m::Limits::none());
                         let t = crate::$m::Type::read_xdr_to_end(r#type, &mut l)?;
@@ -137,23 +137,30 @@ impl Cmd {
     run_x!(run_curr, curr);
     run_x!(run_next, next);
 
-    fn files(&self) -> Result<Vec<Box<dyn Read>>, Error> {
-        if self.files.is_empty() {
+    fn parse_input(&self) -> Result<Vec<Box<dyn Read>>, Error> {
+        if self.input.is_empty() {
             Ok(vec![Box::new(stdin())])
         } else {
-            Ok(self
-                .files
+            Ok(self.input
                 .iter()
-                .map(File::open)
+                .map(|input| match File::open(input) {
+                    Ok(file) => Ok::<Box<dyn Read>, Error>(Box::new(file) as Box<dyn Read>),
+                    Err(e) if e.kind() == ErrorKind::NotFound
+                        // || e.kind() == ErrorKind::InvalidFilename
+                    => {
+                        Ok(Box::new(Cursor::new(input.clone().into_encoded_bytes()))
+                            as Box<dyn Read>)
+                    }
+                    Err(e) => Err(e.into()),
+                })
                 .collect::<Result<Vec<_>, _>>()?
                 .into_iter()
-                .map(|f| -> Box<dyn Read> { Box::new(f) })
                 .collect())
         }
     }
 
     fn out(&self, v: &(impl Serialize + Debug)) -> Result<(), Error> {
-        match self.output {
+        match self.output_format {
             OutputFormat::Json => println!("{}", serde_json::to_string(v)?),
             OutputFormat::JsonFormatted => println!("{}", serde_json::to_string_pretty(v)?),
             OutputFormat::RustDebug => println!("{v:?}"),
